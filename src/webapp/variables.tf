b8a164fc385e2a9fac9cc68b1242be4c14dec3e8
cloudstones <70410709+cloudstones@users.noreply.github.com>
2021-01-01 05:40:23 +0530

    Update variables.tf

--- src/webapp/variables.tf
+++ src/webapp/variables.tf
@@ -1,14 +1,11 @@
 variable "myregion"{
 type = "string"
-default = "us-east-1"
 }
 
 variable "myamiid"{
 type = "string"
-default = "ami-0affd4508a5d2481b"
 }
 
 variable "mykeypair"{
 type = "string"
-default = "virginia"
 }
