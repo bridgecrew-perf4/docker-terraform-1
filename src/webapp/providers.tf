8afb5f5d89126e833cbf762f87e185c57fc4e14e
cloudstones <70410709+cloudstones@users.noreply.github.com>
2021-02-25 01:45:45 +0530

    Update providers.tf

--- src/webapp/providers.tf
+++ src/webapp/providers.tf
@@ -1,7 +1,6 @@
 #########################################  AWS provider #################################
 provider "aws"{
 region = "${var.myregion}"
-shared_credentials_file = "/home/centos/.aws/credentials"
 }
 
 
