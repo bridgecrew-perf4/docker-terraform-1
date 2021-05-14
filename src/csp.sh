cc236a791927c3dda41cc5f71c5937ba32cd346a
Cloud User <centos@ip-172-31-94-98.ec2.internal>
2021-05-14 14:52:32 +0000

    dd

---
+++ src/csp.sh
@@ -0,0 +1,2 @@
+docker image build -t aws1 .
+docker container run -it  -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION aws1
