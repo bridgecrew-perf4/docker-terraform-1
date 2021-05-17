673e93ad475392ea996ea4ccfe3780ce87392c59
Cloud User <centos@ip-172-31-94-98.ec2.internal>
2021-05-17 06:45:34 +0000

    solved

--- src/csp.sh
+++ src/csp.sh
@@ -1,3 +1,7 @@
 mkdir -p environments
-docker image build -t aws1 .
-docker container run -it  -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION aws1
+cmd=$@
+echo $cmd
+docker image build -t awsjyo .
+result="docker container run -i -t -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION -e CSP_CMD=\"${cmd}\" awsjyo"
+#echo $result
+eval "$result"
--- src/packages/env_manager.py
+++ src/packages/env_manager.py
@@ -1,9 +1,12 @@
 import os
-
+import sys
+sys.path.insert(0,'packages')
 CWD = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-print(CWD)
+#print(CWD)
 def create_env(clustername):
+    #CWD = os.path.dirname(os.path.abspath(__file__))
+    print("inside def",clustername)
+    print("inside def",CWD)
     envDir = CWD+"/environments/"+clustername
     os.mkdir(envDir)
-#print(os.path.exists(envDir))
