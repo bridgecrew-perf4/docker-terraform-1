59fdd39f933ac5bc348ea4e81394e9f629a9923c
cloudstones <70410709+cloudstones@users.noreply.github.com>
2021-05-19 19:39:46 +0530

    Update csp.sh

--- src/csp.sh
+++ src/csp.sh
@@ -3,5 +3,4 @@ cmd=$@
 echo $cmd
 docker image build -t awsjyo .
 result="docker container run -i -t -v $PWD/environments:/mesos-terraform/environments-v $PWD/clusters:/mesos-terraform/clusters -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION -e CSP_CMD=\"${cmd}\" awsjyo"
-#echo $result
 eval "$result"
