5541eb87f4d9b19c814a8c7e0253f54e851937e3
Cloud User <centos@ip-172-31-26-151.ec2.internal>
2021-05-28 16:22:40 +0000

    update

--- src/packages/env_manager.py
+++ src/packages/env_manager.py
@@ -2,11 +2,17 @@ import os
 import sys
 import shutil
 import cluster_manager
+import terraform
 sys.path.insert(0,'packages')
 
 def create_env(clustername):
     CSP_BASE = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     envDir = CSP_BASE+"/environments/"+clustername
     shutil.copytree(CSP_BASE+"/providers/"+"/aws/",envDir)
+def apply_env(clustername):
+    status = terraform.terraform_apply(clustername)
+def destroy_env(clustername):
+    print("terraform_destroy")
+    status = terraform.terraform_destroy(clustername)
 def cleanup_env(clustername):
     status = cluster_manager.delete_cluster(clustername)
--- src/packages/terraform.py
+++ src/packages/terraform.py
@@ -2,11 +2,20 @@
 
 import subprocess
 
+
 cmds = ["terraform  init providers/aws/ && terraform validate -var-file=clusters/aws/dev.json providers/aws && terraform apply -var-file=clusters/aws/dev.json -auto-approve providers/aws"]
+cmds_destroy = ["terraform  init providers/aws/ && terraform validate -var-file=clusters/aws/dev.json providers/aws && terraform destroy -var-file=clusters/aws/dev.json -auto-approve providers/aws"]
 
-for cmd in cmds:
-    print("command name", cmd)
-    cmd_execution = subprocess.Popen(cmd, stdout = subprocess.PIPE, stderr = subprocess.STDOUT, shell = True)
-    cmd_output, cmd_error = cmd_execution.communicate()
-    print("output of the command", cmd_output)
+def terraform_apply(clustername):
+    for cmd in cmds:
+        print("command name", cmd)
+        cmd_execution = subprocess.Popen(cmd, stdout = subprocess.PIPE, stderr = subprocess.STDOUT, shell = True)
+        cmd_output, cmd_error = cmd_execution.communicate()
+        print("output of the command", cmd_output)
 
+def terraform_destroy(clustername):
+    for cmd in cmds_destroy:
+        print("command name", cmd)
+        cmd_execution = subprocess.Popen(cmd, stdout = subprocess.PIPE, stderr = subprocess.STDOUT, shell = True)
+        cmd_output, cmd_error = cmd_execution.communicate()
+        print("output of the command", cmd_output)
