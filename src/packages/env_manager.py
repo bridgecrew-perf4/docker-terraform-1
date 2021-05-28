6cff935dea742ed6309cc06055e85aca337167ba
Cloud User <centos@ip-172-31-26-151.ec2.internal>
2021-05-28 04:56:47 +0000

    ss

--- src/packages/env_manager.py
+++ src/packages/env_manager.py
@@ -1,8 +1,12 @@
 import os
 import sys
 import shutil
+import cluster_manager
 sys.path.insert(0,'packages')
+
 def create_env(clustername):
     CSP_BASE = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     envDir = CSP_BASE+"/environments/"+clustername
     shutil.copytree(CSP_BASE+"/providers/"+"/aws/",envDir)
+def cleanup_env(clustername):
+    status = cluster_manager.delete_cluster(clustername)
